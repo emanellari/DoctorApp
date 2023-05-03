58c4cac60ea0b7ea9c3e238f1d59a2f3fc04abb8
emanellari <132411421+emanellari@users.noreply.github.com>
2023-05-03 03:18:52 +0200

    Add files via upload

---
+++ ConsoleApp.Test/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using ConsoleApp.Test;
+
+Console.WriteLine("----------------- START: CLASS ---------------------");
+
+var patient = new Patient();
+patient.FirstName = "Patient Fullname";
+Console.WriteLine($"Patient FistName [patient]: {patient.FirstName}");
+
+string patientFullName = patient.GetFullName("Taulant", "Tahiri");
+Console.WriteLine($"Patient Full Name [patient]: {patientFullName}");
+
+// 
+string patientFullNameStatic = PatientStatic.GetFullName(firstName: "Taulant",
+                                                         lastName: "Tahiri");
+Console.WriteLine($"Patient Full Name [patient]: {patientFullNameStatic}");
+
+//var lastPatient = new Patient("Patient Fullname 2");
+//Console.WriteLine($"Patient FistName [lastPatient]: {lastPatient.FirstName}");
+
+
+Console.WriteLine("----------------- START: CLASS ---------------------");
+
+
+
+
+
+
+
+
