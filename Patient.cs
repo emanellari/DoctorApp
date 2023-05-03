0f12ea6bd8648dc2244a1cc91b4b93243aa9788e
emanellari <132411421+emanellari@users.noreply.github.com>
2023-05-03 03:50:50 +0200

    Add files via upload

---
+++ Patient.cs
@@ -0,0 +1,35 @@
+﻿using ConsoleApp.Test.Interfaces;
+
+namespace ConsoleApp.Test
+{
+    public class Patient : IDataService //BaseClass
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string GetDataFromDatabase()
+        {
+            //throw new NotImplementedException();
+
+            //MYSQL
+
+            return "";
+        }
+
+        public string GetFullName(string firstName, string lastName)
+        {
+            return $"{firstName} {lastName}";
+        }
+
+
+
+        //public int GetAge()
+        //{
+        //    var age = DateTime.Now.Year - DateOfBirth.Year;
+        //    return age;
+        //}
+
+
+    }
+}
+
---
+++ PatientStatic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.Test
+{
+    public static class PatientStatic
+    {
+        public static string FirstName { get; set; }
+        public static string LastName { get; set; }
+
+        public static string GetFullName(string firstName, string lastName)
+        {
+            return $"{firstName} {lastName}";
+        }
+    }
+}
+
